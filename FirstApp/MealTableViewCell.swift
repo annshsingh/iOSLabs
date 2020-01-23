64cf3bfdbfb4bf3d8db7c69353f28578812e175d
Annsh Singh <annsh@so.city>
2020-01-23 18:12:42 +0530

    Added table view controller to display meals. Populated the table view with static data.

---
+++ FirstApp/MealTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  MealTableViewCell.swift
+//  FirstApp
+//
+//  Created by Annsh Singh on 23/01/20.
+//  Copyright © 2020 Annsh Singh. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    //MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
